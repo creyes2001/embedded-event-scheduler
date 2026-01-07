ff6d3a69008cb9d56c16adb4d8a759f59c63b630
creyes2001 <reyescristopher43@gmail.com>
2026-01-07 13:53:20 -0600

    add gpio_driver skeleton

---
+++ inc/gpio_driver.h
@@ -0,0 +1,28 @@
+#ifndef GPIO_H
+#define GPIO_H
+
+#include <stdint.h>
+
+typedef struct{
+	volatile uint8_t *port,
+	volatile uint8_t *tris,
+	volatile uint8_t *lat,
+	uint8_t pin
+}gpio_t;
+
+typedef enum{
+	GPIO_OUTPUT,
+	GPIO_INPUT
+}gpio_dir_e;
+
+typedef enum{
+	GPIO_LOW,
+	GPIO_HIGH
+}gpio_level_e;
+
+void gpio_init(gpio_t *gpio,gpio_dir_e dir);
+void gpio_write(gpio_t *gpio,gpio_level_e level);
+gpio_level_e gpio_read(gpio_t *gpio);
+void gpio_toggle(gpio_t *gpio);
+
+#endif
---
+++ src/gpio_driver.c
@@ -0,0 +1,22 @@
+#include "gpio_driver.h"
+
+
+void gpio_init(gpio_t *gpio,gpio_dir_e dir)
+{
+	//TODO: logic
+}
+
+void gpio_write(gpio_t *gpio,gpio_level_e level)
+{
+	//TODO:logic
+}
+
+gpio_level_e gpio_read(gpio_t *gpio)
+{
+	//TODO: logic
+}
+
+void gpio_toggle(gpio_t *gpio)
+{
+	//TODO: logic
+}
