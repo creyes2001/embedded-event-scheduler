ec94df99a45cab577ff7b71dec75f720e9e10da0
creyes2001 <reyescristopher43@gmail.com>
2026-01-08 13:53:48 -0600

    add led_driver skeleton

---
+++ inc/led_driver.h
@@ -0,0 +1,25 @@
+#ifndef LED_H
+#define LED_H
+
+#include "gpio_driver.h"
+
+typedef enum{
+	ACTIVE_LOW,
+	ACTIVE_HIGH
+}led_polarity_e;
+
+typedef enum{
+	LED_OFF,
+	LED_ON
+}led_state_e;
+
+typedef struct{
+	gpio_t *gpio;
+	led_polarity_e polarity;
+}led_t;
+
+void led_init(led_t *led);
+void led_set(led_t *led,led_state_e state);
+void led_toggle(led_t *led);
+
+#endif
---
+++ src/led_driver.c
@@ -0,0 +1,20 @@
+
+#include "gpio_driver.h"
+#include "led_driver.h"
+
+void led_init(led_t *led)
+{
+	//TODO: implement
+}
+
+void led_set(led_t *led,led_state_e state)
+{
+	//TODO: implement
+}
+
+void led_toggle(led_t *led)
+{
+	//TODO: implement
+}
+
+
