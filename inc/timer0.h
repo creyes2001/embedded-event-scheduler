fdbb0f5aaef0e3c076a65bfb9004d8cb5f759630
creyes2001 <reyescristopher43@gmail.com>
2026-01-13 14:21:04 -0600

    feat: add timer 0 configuration for tick generation

---
+++ inc/timer0.h
@@ -0,0 +1,11 @@
+#ifndef TIMER0_H
+#define TIMER0_H
+
+#include <stdint.h>
+
+void timer0_init(void);
+void timer0_start(void);
+void timer0_stop(void);
+void timer0_reload(void);
+
+#endif
---
+++ src/timer0.c
@@ -0,0 +1,35 @@
+#include <xc.h>
+#include "timer0.h"
+
+#define TIMER0_RELOAD 0xFB1E
+
+void timer0_init(void)
+{
+	T0CON = 0x00;			//stop timer
+	T0CONbits.T08BIT = 0;	//16 bits
+	T0CONbits.T0CS = 0;		//FOSC/4
+	T0CONbits.PSA = 0;		//prescaler enable
+	T0CONbits.T0PS = 0b001;	//prescaler 1:4
+							
+	timer0_reload();
+
+	INTCONbits.TMR0IF = 0;
+	INTCONbits.TMR0IE = 1;
+}
+
+void timer0_start(void)
+{
+	T0CONbits.TMR0ON = 1;
+}
+
+void timer0_stop(void)
+{
+	T0CONbits.TMR0ON = 0;
+}
+
+void timer0_reload(void)
+{
+	TMR0H = (TIMER0_RELOAD << 8) & 0xFF;
+	TMR0L = TIMER0_RELOAD & 0xFF;
+}
+
